128cabcac226527d0e0ff6a92a08b56ae09bf671
Liza <lizaamatya@gmail.com>
2023-02-24 23:36:33 +0545

    read unread email

--- email_app/urls.py
+++ email_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from email_app.views import EmailList, SendEmail
+from email_app.views import EmailList, SendEmail, MarkReadUnread
 
 app_name = 'email'
 
 urlpatterns = [
     path('send', SendEmail.as_view(), name='send_email'),
-    path('', EmailList.as_view(), name='email-list')
+    path('', EmailList.as_view(), name='email-list'),
+    path('read/<int:pk>', MarkReadUnread.as_view(), name='mark-read')
 ]
--- email_app/views.py
+++ email_app/views.py
@@ -1,32 +1,37 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django_filters.rest_framework import DjangoFilterBackend
+from emailsystem.settings import EMAIL_HOST_USER, TEMPLATE_DIR
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from email_app.filters import EmailFilter
 from email_app.models import Email
 from email_app.serializers import EmailSerializer
 from email_app.tasks import send_dynamic_email
-from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from emailsystem.settings import TEMPLATE_DIR, EMAIL_HOST_USER
-from rest_framework.pagination import PageNumberPagination
 
 
 class SendEmail(CreateAPIView):
     model = Email
     serializer_class = EmailSerializer
     permission_classes = [AllowAny]
-    
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         instance = serializer.save(sender_email=EMAIL_HOST_USER, status='sent')
         context = serializer.validated_data
-       
+
         template = None
         if instance.email_template:
             template = instance.email_template.pk
-            
+
         context.pop('email_template')
-        send_email = send_dynamic_email.delay(instance.pk, template, dict(context))
+        send_email = send_dynamic_email.delay(
+            instance.pk, template, dict(context))
         headers = self.get_success_headers(serializer.data)
         return Response({'message': "Email sent."}, status=status.HTTP_201_CREATED, headers=headers)
 
@@ -35,6 +40,31 @@ class EmailList(ListAPIView):
     serializer_class = EmailSerializer
     queryset = Email.objects.all()
     pagination_class = PageNumberPagination
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend]
     filtset_class = EmailFilter
-    
+    # filterset_fields = ['recipient_emails']
+    permission_classes = [AllowAny]
+
+
+class MarkReadUnread(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+
+        try:
+            instance = Email.objects.get(id=self.kwargs['pk'])
+            instance.is_read = not instance.is_read
+            instance.save()
+
+            if instance.is_read:
+                message = 'Email marked read'
+            else:
+                message = 'Email marked unread'
+                
+            code = status.HTTP_200_OK
+
+        except ObjectDoesNotExist:
+            message = f"Email object with id {self.kwargs['id']} does not exist."
+            code = status.HTTP_404_NOT_FOUND
+
+        return Response({"msg": message}, status=code)
