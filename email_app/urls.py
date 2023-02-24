2a8aa6e0dc7720bbabf90e18cc2a5bf032a52834
Liza <lizaamatya@gmail.com>
2023-02-24 22:38:36 +0545

    query by date range and email addresses

--- email_app/urls.py
+++ email_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from email_app.views import SendEmail
+from email_app.views import EmailList, SendEmail
 
 app_name = 'email'
 
 urlpatterns = [
-    path('send', SendEmail.as_view(), name='send_email')
+    path('send', SendEmail.as_view(), name='send_email'),
+    path('email-list', EmailList.as_view(), name='email-list')
 ]
